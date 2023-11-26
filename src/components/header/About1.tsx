02f6a76ee36a11cd9480fac32ff51cb7af1f5307
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-11-27 07:11:35 +0800

    create header/About1

---
+++ src/components/header/About1.tsx
@@ -0,0 +1,43 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import Image from '/public/header/header1.jpg?jsx'
+
+export default component$(() => {
+  useStylesScoped$(`
+    img {
+      width: 100%;
+      height: auto;
+      display: block;
+      margin-top: 100px;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      margin-top: 2em;
+      gap: 1em;
+    }
+
+    p {
+      max-width: 38.125em;
+    }
+
+    @media (width >= 1000px) {
+      div {
+        flex-direction: row;
+        gap: 5em;
+      }
+
+      h1 { flex-shrink: 0; }
+    }
+  `)
+  
+  return (
+    <header class="">
+      <Image alt="members of the team" loading="eager"/>
+      <div class="fluid-header">
+        <h1>Visualize Your Dream Space:<br />We're Your Creative Team</h1>
+        <p>At FT2, we're your dedicated partners in creating spaces that truly reflect your lifestyle and vision. Our award-winning interior designers are ready to bring your dream living space to life. With FT2, your living space isn't just transformed; it's designed to complement your life seamlessly. We're here to make your vision a reality, ensuring a hassle-free and enjoyable journey.</p>
+      </div>
+    </header>
+  )
+})
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import About1 from "~/components/header/About1";
+
 export default component$(() => {
   return (
     <>
+      <About1 />
       <h1>Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
