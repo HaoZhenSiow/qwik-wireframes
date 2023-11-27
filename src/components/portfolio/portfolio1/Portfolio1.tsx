022bf91afbc3018d680298ab52e42b0c1614baa2
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-11-28 06:43:31 +0800

    create Portfolio1

---
+++ src/components/portfolio/portfolio1/Portfolio1.tsx
@@ -0,0 +1,57 @@
+import { component$,useStylesScoped$ } from '@builder.io/qwik'
+
+import Project1 from './Project1'
+
+export default component$(() => {
+  useStylesScoped$(`
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    h2 {
+      align-self: start;
+    }
+
+    div {
+      display: grid;
+      gap: 1.5em;
+      grid-template-columns: repeat(1, minmax(0, 1fr));
+
+      @media (width > 650px) {
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+      }
+
+      @media (width > 1200px) {
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+      }
+    }
+
+    button {
+      font-weight: bold;
+      padding: 1em 2em;
+      margin-top: 2em;
+      background-color: black;
+      color: white;
+      border-radius: 5px;
+    }
+  `)
+
+  return (
+    <section class="fluid-section" aria-label="portfolio">
+      <h2>Outstanding Work and Nothing Less</h2>
+      {/* <h2 class="text-center">Outstanding Work<br class="gte650:hidden" /> and Nothing Less</h2> */}
+      {/* <h2>Find Out What <br/>We Do Best</h2> */}
+      <div>
+        <Project1 />
+        <Project1 />
+        <Project1 />
+        <Project1 />
+        <Project1 />
+        <Project1 />
+      </div>
+      <button type="button">All Projects</button>
+    </section>
+  )
+})
--- src/template/landing-page/landing-page-1/LandingPage1.tsx
+++ src/template/landing-page/landing-page-1/LandingPage1.tsx
@@ -4,6 +4,7 @@ import DoubleMarquee from "~/components/hero/DoubleMarquee"
 import Feature1 from "./feature/Feature1"
 import Feature2 from "./feature/Feature2"
 import LogoMarquee from "~/components/ultil/marquee/LogoMarquee"
+import Portfolio1 from "~/components/portfolio/portfolio1/Portfolio1"
 
 export default component$(() => {
   return (
@@ -13,6 +14,7 @@ export default component$(() => {
         <LogoMarquee />
         <Feature1 />
         <Feature2 />
+        <Portfolio1 />
       </main>
     </>
   )
