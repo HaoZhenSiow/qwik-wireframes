c1817fb4f76192ec21702a6dbc1aa127a725282b
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-11-28 06:38:52 +0800

    create marquee/LogoMarquee

--- src/template/landing-page/landing-page-1/LandingPage1.tsx
+++ src/template/landing-page/landing-page-1/LandingPage1.tsx
@@ -3,13 +3,17 @@ import { component$ } from "@builder.io/qwik"
 import DoubleMarquee from "~/components/hero/DoubleMarquee"
 import Feature1 from "./feature/Feature1"
 import Feature2 from "./feature/Feature2"
+import LogoMarquee from "~/components/ultil/marquee/LogoMarquee"
 
 export default component$(() => {
   return (
     <>
       <DoubleMarquee />
-      <Feature1 />
-      <Feature2 />
+      <main>
+        <LogoMarquee />
+        <Feature1 />
+        <Feature2 />
+      </main>
     </>
   )
 })
